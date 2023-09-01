52bae9a98b43301d788f9daa4f95795c968712ca
petar-georgiev <georgiev.peter1@gmail.com>
2023-09-01 17:54:46 +0300

    [SE-4] Implemented UserRestController.

---
+++ scale_events_web/src/main/java/com/scale_events/web/controller/IndexRestController.java
@@ -0,0 +1,12 @@
+package com.scale_events.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexRestController {
+    @RequestMapping("/")
+    public String index() {
+        return "Welcome to the index!";
+    }
+}
